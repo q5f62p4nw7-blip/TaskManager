9be0a3a52734f7e0cba11e941ac9d1eddc06cc00
İrem Kılıçtaş <iremkilictas26@icloud.com>
2025-12-25 13:45:54 +0300

    Add Task class and Completable interface

---
+++ iremProject/src/main/Completable.java
@@ -0,0 +1,13 @@
+package main;
+
+// Bu interface, bir görevin tamamlanabilmesini sağlar
+// Bu arayüzü kullanan sınıflar complete() metodunu yazmak zorundadır.
+public interface Completable {
+
+    // Tamamlama işlemini temsil eder
+    void complete();
+
+
+}
+
+
---
+++ iremProject/src/main/Task.java
@@ -0,0 +1,47 @@
+package main;
+
+// Bu sınıf, yapılacak bir görevi temsil eder
+// Task(görev) tamamlanabilir olduğu için Completable interface'ini kullanır
+
+public class Task implements Completable {
+    
+    // Encapsulation (private alanlar)
+    private String title;  // Görevin adı
+    private String description;  // Görevin açıklaması
+    private boolean completed;  // Görev bitti mi bitmedi mi?
+
+    // Constructor
+    // Task nesnesi oluşturulurken görev adı, açıklama ve başlangıç durumunu ayarlamak için kullanılır.
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    // Getter'lar
+    // Görevin bilgisini dışarıya vermek için kullanılır.
+
+    // Görevin adını verir
+    public String getTitle() {
+        return title;
+    }
+
+    // Görevin açıklamasını verir
+    public String getDescription() {
+        return description;
+    }
+
+    // Görev tamamlandı mı?
+    // Boolean’larda get yerine is kullanılır (Java kuralı)
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    // Interface metodu
+    // Görevi tamamlandı olarak işaretler
+    @Override
+    public void complete() {
+        this.completed = true; //Bu görev artık tamamlandı
+        System.out.println("Görev tamamlandı: " + title); // kullanıcıya bilgi verir
+    }
+}
