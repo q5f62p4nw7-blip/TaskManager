4eb32e761aca531b76bb1e0cb7890f784a3767fe
İrem Kılıçtaş <iremkilictas26@icloud.com>
2026-01-02 23:34:25 +0300

    Add Notification class

---
+++ iremProject/src/taskmanager/Notification.java
@@ -0,0 +1,11 @@
+package taskmanager;
+
+// Bu sınıf, kullanıcıya basit bildirim göndermek için kullanılır
+public class Notification {
+
+    // Kullanıcıya mesaj gönderir
+    public void send(String message) {
+        System.out.println("Notification: " + message);
+    }
+}
+
